0d02650a241093099f5a31f0756b1c1dd7a60617
Rafael-Creator-max <rafael.pereztarira@student.kuleuven.be>
2025-06-10 23:11:01 +0200

    changes on cors

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -12,7 +12,15 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->web(\Illuminate\Session\Middleware\StartSession::class);
+        $middleware->api(\Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class);
+        
+        $middleware->validateCsrfTokens(except: [
+            '*',
+        ]);
+        
+        $middleware->trustProxies(at: '*');
+        $middleware->trustHosts(at: ['*']);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
